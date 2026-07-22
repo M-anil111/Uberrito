25251db2d6656ddcdf6bbeb69922f3ceca6113ca
Jay Mehta <jay@jaymehta.co>
2026-07-22 12:09:06 -0500

    Match approved hero contrast and mobile order CTA

--- wp-content/plugins/uberrito-new-home/uberrito-new-home.php
+++ wp-content/plugins/uberrito-new-home/uberrito-new-home.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Uberrito New Home Experience
  * Description: Isolated /new-home/ redesign and motion system for staging review.
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Uberrito
  */
 
